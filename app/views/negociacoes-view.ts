1a12c0ae023e0b0ffb75c6c98c3db2f245b47310
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-05-03 15:34:44 -0300

    Sixth class of typescript 1.0

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,26 @@
+export class NegociacoesView {
+
+    private elemento: HTMLElement;
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    template(): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+        </table>
+
+        `;
+    }
+
+    update(): void {
+        this.elemento.innerHTML = this.template();
+    }
+}
