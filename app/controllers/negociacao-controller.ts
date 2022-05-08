2913efd6848a346fd177e13b3f92d7892d42c3f8
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-05-08 20:39:19 -0300

    Finished Eight Class

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -2,6 +2,7 @@ import { MensagemView } from './../views/mensagem-view.js';
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -10,6 +11,7 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
+
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -21,12 +23,20 @@ export class NegociacaoController {
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update('Apenas negociações em dias úteis, serão aceitas');
+            return ; 
+        }
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
         this.mensagemView.update('Negociação adicionada com sucesso');
         this.limparFormulario();
-        
     }
+
+     private ehDiaUtil(data: Date) {
+         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
+     }
+        
     criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ","));
@@ -39,7 +49,4 @@ export class NegociacaoController {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
-        this.inputData.focus();
-
-    }
-}
+        this.inputData.focus();}}
