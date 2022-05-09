b6c00e02cf5836773d1c64e59ba2103242148adb
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-05-09 13:16:08 -0300

    Beginning of Eleventh class of Typescript

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -14,7 +14,7 @@ export class NegociacaoController {
 
 
     constructor() {
-        this.inputData = document.querySelector('#data') as HTMLInputElement;
+        this.inputData = <HTMLInputElement>document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
         this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
