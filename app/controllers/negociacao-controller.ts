92f513c3f2cb4fb50f572552b8322b20c714eef0
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-05-08 21:26:35 -0300

    Middle of ninth class

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -21,8 +21,11 @@ export class NegociacaoController {
 
     }
 
-    adiciona(): void {
-        const negociacao = this.criaNegociacao();
+    public adiciona(): void {
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value);
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update('Apenas negociações em dias úteis, serão aceitas');
             return ; 
@@ -37,14 +40,6 @@ export class NegociacaoController {
          return data.getDay() > DiasDaSemana.DOMINGO 
             && data.getDay() < DiasDaSemana.SABADO;
      }
-        
-    criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ","));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
-    }
 
     limparFormulario(): void{
         this.inputData.value = '';
