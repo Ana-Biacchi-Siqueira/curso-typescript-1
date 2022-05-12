c6aa3aaa1d300bb0659a25305257433bcbe0f949
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-05-12 12:57:20 -0300

    Finished Eleventh Class of TS

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -14,7 +16,7 @@ export abstract class View<T> {
             this.escapar = escapar;
         }
     }
-
+    @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
