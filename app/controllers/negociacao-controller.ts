00c2ecada8b21a46608b9c244f9a5f5aeeccb9ac
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-05-22 20:04:49 -0300

    Finished fifteenth class of TS

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -6,6 +6,7 @@ import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { inspect } from "../decorators/inspect.js";
 import { domInjector } from "../decorators/dom-injector.js";
+import { NegociacoesService } from '../services/negociacoes-service.js';
 
 export class NegociacaoController {
   @domInjector("#data")
@@ -17,6 +18,7 @@ export class NegociacaoController {
   private negociacoes = new Negociacoes();
   private negociacoesView = new NegociacoesView("#negociacoesView");
   private mensagemView = new MensagemView("#mensagemView");
+  private negociacoesService = new NegociacoesService();
 
   constructor() {
     this.negociacoesView.update(this.negociacoes);
@@ -44,22 +46,13 @@ export class NegociacaoController {
   }
 
   importaDados(): void {
-    fetch("http://localhost:8080/dados")
-      .then((res) => res.json())
-      .then((dados: any[]) => {
-        return dados.map((dadoDeHoje) => {
-          return new Negociacao(
-            new Date(),
-            dadoDeHoje.vezes,
-            dadoDeHoje.montante
-          )
-        })
-      })
-      .then(negociacoesDeHoje => {
-        for(let negociacao of negociacoesDeHoje) {
-          this.negociacoes.adiciona(negociacao);
+    this.negociacoesService
+        .obterNegociacoesDoDia()
+        .then(negociacoesDeHoje => {
+          for(let negociacao of negociacoesDeHoje) {
+            this.negociacoes.adiciona(negociacao);
         }
-        this.negociacoesView.update(this.negociacoes);
+    this.negociacoesView.update(this.negociacoes);
       });
   }
 
